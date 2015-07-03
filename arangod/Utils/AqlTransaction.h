6f5382a5449f9f089e5597c4ca7654118582a06c
Michael Hackstein <michael@arangodb.com>
2015-07-03 12:55:58 +0200

    Added an assertion for collections not known to the collection

--- arangod/Utils/AqlTransaction.h
+++ arangod/Utils/AqlTransaction.h
@@ -155,6 +155,7 @@ namespace triagens {
 
         triagens::arango::DocumentDitch* ditch (TRI_voc_cid_t cid) {
           TRI_transaction_collection_t* trxColl = this->trxCollection(cid);
+          TRI_ASSERT(trxColl != nullptr);
           return trxColl->_ditch;
         }
 
@@ -164,6 +165,7 @@ namespace triagens {
 
         TRI_document_collection_t* documentCollection (TRI_voc_cid_t cid) {
           TRI_transaction_collection_t* trxColl = this->trxCollection(cid);
+          TRI_ASSERT(trxColl != nullptr);
           return trxColl->_collection->_collection;
         }
 
